60b494cd7481239a4f381b3873f5fedb5d392a34
ksyasuda <ksyasuda@umich.edu>
2021-03-05 17:31:15 -0500

    got chess outputting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MyBoard from './components/board/board';
 
-function App() {
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section style={boardStyle}>
+      <MyBoard />
+    </section>
   );
-}
+};
 
-export default App;
+const boardStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexWrap: 'wrap',
+  marginTop: '5%',
+};
+
+export default app;
